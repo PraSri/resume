6a7d9908c5d878a8a426a3a866ecf7e4704c1631
Prakhar Srivastava <prasri510@gmail.com>
2025-03-26 01:49:08 +0530

    New resume part 2

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 docker build -t sb2nov/latex .
 docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex prakhar_resume.tex
+
+
+docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex prakhar_resume2.tex
