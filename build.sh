0da40b7fe3aaa351bab079bc02866fa84667c0ea
Prakhar Srivastava <prasri510@gmail.com>
2025-07-27 23:26:32 +0530

    version 3

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex pra
 
 
 docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex prakhar_resume2.tex
+
+
+
+docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex resumePer.tex
