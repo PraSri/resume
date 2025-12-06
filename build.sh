da0b182380c4cfb351704b30e61b0fe09eb2b3bf
prakharsrivastava <prasri510@gmail.com>
2025-12-06 20:57:52 +0530

    Resume updated

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 docker build -t sb2nov/latex .
-docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex prakhar_resume.tex
+#docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex prakhar_resume.tex
+#
+#
+#docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex prakhar_resume2.tex
+#
+#
+#
+#docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex resumePer.tex
 
+docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume4.tex
 
-docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex prakhar_resume2.tex
-
-
-
-docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex resumePer.tex
+#docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex sourabh_bajaj_resume.tex
