9fd7fc8aa29baa0974626fa2bb548053d482d408
prakharsrivastava <prasri510@gmail.com>
2025-12-30 15:22:53 +0530

    spare resume

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ docker build -t sb2nov/latex .
 #
 #docker run --rm -i -v "D:\MyProjects2025\resume":/data sb2nov/latex pdflatex resumePer.tex
 
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume4.tex
+docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume_coup.tex
 
 #docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex sourabh_bajaj_resume.tex
