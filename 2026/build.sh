c83447e74872af4b887370432d7e14aefc5961f0
prakharsrivastava <prasri510@gmail.com>
2026-06-08 16:35:48 +0530

    resume5.1

--- 2026/build.sh
+++ 2026/build.sh
@@ -12,8 +12,8 @@ docker build -t sb2nov/latex .
 
 # docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume_coup.tex
 
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume5.tex
+docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume5.1.tex
 
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume6.tex
+#docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex prakhar_resume6.tex
 
 #docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex sourabh_bajaj_resume.tex
